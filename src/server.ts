49cafc6d66789b0cfaec38104fddcdc16ac5b67b
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2023-12-05 18:10:39 +0100

    custom logger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import http from "http";
 import { config } from "./config";
 import createServer from "./utils/app";
+import log from "./utils/logger";
 
 const app = createServer();
 
 http.createServer(app).listen(config.server.port, () => {
-  console.log(`Server is running on port: ${config.server.port}`);
+  log.info(`Server is running on port: ${config.server.port}`);
 });
