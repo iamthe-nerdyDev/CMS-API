5a1c49dfb9388feaa9100c443e6e5f8eac247a67
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2023-12-07 11:40:01 +0100

    healthcheck route and options handling

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -7,9 +7,17 @@ import sessionRoutes from "../routes/session.routes";
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+
+  next();
+});
+
 router.use("/user", userRoutes);
 router.use("/category", categoryRoutes);
 router.use("/post", postRoutes);
 router.use("/session", sessionRoutes);
+
+router.get("/healthcheck", (_, res) => res.sendStatus(200));
 
 export = router;
